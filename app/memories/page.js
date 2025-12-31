6601eeb1ecf7ba2f5abeb1c34d5a69210e6dc55c
Shruti Misal <shruti.private10@gmail.com>
2025-12-31 20:21:53 +0530

    its the end

--- app/memories/page.js
+++ app/memories/page.js
@@ -1,7 +1,7 @@
-import MemoriesClient from "./MemoriesClient";
+import MemoriesClient from "./MemorieClient";
 
 export default async function MemoriesPage({ searchParams }) {
-  const params = await searchParams;      // ✅ unwrap Promise
+  const params = await searchParams;
   const name = params?.name?.toLowerCase();
 
   return <MemoriesClient name={name} />;
